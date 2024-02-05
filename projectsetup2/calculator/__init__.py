104f8cd58b0cd90092042bb830a1a7168954c8d1
jatamel <jm2449@njit.edu>
2024-02-04 23:11:44 -0500

    created a calculator file

---
+++ projectsetup2/calculator/__init__.py
@@ -0,0 +1,9 @@
+'''this is a calculator'''
+
+def add(a,b):
+    '''so i need a docstring here too ig..'''
+    return a + b
+
+def subtract(a,b):
+    '''so this is how we subtract..'''
+    return a - b
